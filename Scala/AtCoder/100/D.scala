bb8a6fd348fd9df229d53fa8a7bd5df69354912f
Kuropikumin <wdbsf273@yahoo.co.jp>
2019-09-18 02:39:46 +0900

    add AtCoder Beginner Contest Probrem D

---
+++ Scala/AtCoder/100/D.scala
@@ -0,0 +1,8 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+    val factors = scala.io.StdIn.readLine.split(" ").Map{_.toInt}
+    var scores: 
+    for(line <- scala.io.Source.StdIn.getLines()) {
+    }
+  }
+}
