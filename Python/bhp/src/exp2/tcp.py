54dfebd2a2b55026eb29339b07cff95e49034c3c
Kuropikumin <wdbsf273@yahoo.co.jp>
2022-05-06 17:41:17 +0900

    ■サイバーセキュリティプログラミング第2版第2章追加   - Python/bhp/src/exp2/*

---
+++ Python/bhp/src/exp2/tcp.py
@@ -0,0 +1,19 @@
+import socket
+
+target_host = "127.0.0.1"
+target_port = 9998
+
+# socketオブジェクトの作成
+client = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+
+# サーバへの接続
+client.connect( ( target_host, target_port ) )
+
+# データの送信
+client.send( b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\nAABBCC" )
+
+# データの受信
+response = client.recv( 4096 )
+
+print( response.decode() )
+client.close()
